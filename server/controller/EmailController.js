5c549a84aeaa75aab1c059aa521987554d59fd89
paulinCiany <paulinlubamba24@gmail.com>
2024-03-28 15:28:59 +0200

    some modifications made to email controller procedural to class

--- server/controller/EmailController.js
+++ server/controller/EmailController.js
@@ -1,15 +1,15 @@
 import emailModel from '../model/EmailModel';
 
-async function getEmails(req, res) {
-    try {
-        const emails = await emailModel.fetchEmails();
-        res.json(emails);
-    } catch (err) {
-        console.error('Error fetching emails:', err);
-        res.status(500).json({ error: 'Error fetching emails' });
+class emailController {
+    static async getEmails(req, res) {
+        try {
+            const emails = await emailModel.fetchEmails();
+            res.json(emails);
+        } catch (err) {
+            console.error('Error fetching emails:', err);
+            res.status(500).json({ error: 'Error fetching emails' });
+        }
     }
 }
 
-module.exports = {
-    getEmails
-};
+module.exports = emailController;
