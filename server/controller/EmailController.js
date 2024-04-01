4f587847c862a058bd3fe4c57bf88bd02e890a22
paulinCiany <paulinlubamba24@gmail.com>
2024-04-01 23:06:56 +0200

    email not finished

--- server/controller/EmailController.js
+++ server/controller/EmailController.js
@@ -4,7 +4,7 @@ import  fetchEmails  from '../model/EmailModel.js';
 const getEmails = async (req, res) => {
     try {
         const emails = await fetchEmails();
-        res.json(emails);
+        res.json({ success: true, emails });
     } catch (error) {
         res.status(500).json({ success: false, message: 'Error fetching emails', error: error.message });
     }
