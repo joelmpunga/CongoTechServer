8435e0bb71e6ca5cbeaf9e0798c3dfd034ad5a0b
paulinCiany <paulinlubamba24@gmail.com>
2024-04-03 15:32:41 +0200

    handleDrop function added

--- client/src/components/DragDrop/CustomContainer.jsx
+++ client/src/components/DragDrop/CustomContainer.jsx
@@ -10,11 +10,78 @@ export function CustomDragDrop({
   count,
   formats
 }) {
+
+    const dropContainer = useRef(null);
+    const [dragging, setDragging] = useState(false);
+    const fileRef = useRef(null);
+
+    function handleDrop(e, type) {
+        let files;
+        if (type === "inputFile") {
+          files = [...e.target.files];
+        } else {
+          e.preventDefault();
+          e.stopPropagation();
+          setDragging(false);
+          files = [...e.dataTransfer.files];
+        }
+    
+        const allFilesValid = files.every((file) => {
+          return formats.some((format) => file.type.endsWith(`/${format}`));
+        });
+    
+        if (ownerLicense.length >= count) {
+          showAlert(
+            "warning",
+            "Maximum Files",
+            `Only ${count} files can be uploaded`
+          );
+          return;
+        }
+        if (!allFilesValid) {
+          showAlert(
+            "warning",
+            "Invalid Media",
+            `Invalid file format. Please only upload ${formats
+              .join(", ")
+              .toUpperCase()}`
+          );
+          return;
+        }
+        if (count && count < files.length) {
+          showAlert(
+            "error",
+            "Error",
+            `Only ${count} file${count !== 1 ? "s" : ""} can be uploaded at a time`
+          );
+          return;
+        }
+    
+        if (files && files.length) {
+          const nFiles = files.map(async (file) => {
+            const base64String = await convertFileBase64(file);
+            return {
+              name: file.name,
+              photo: base64String,
+              type: file.type,
+              size: file.size
+            };
+          });
+    
+          Promise.all(nFiles).then((newFiles) => {
+            onUpload(newFiles);
+            TopNotification.fire({
+              icon: "success",
+              title: "file uploaded"
+            });
+          });
+        }
+      }
  
 
   return (
     <>
-        
+
 
 
     </>
