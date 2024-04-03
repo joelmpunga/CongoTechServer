95eac94f05338a48e6e657f341e203f71b8a0685
paulinCiany <paulinlubamba24@gmail.com>
2024-04-03 15:48:56 +0200

    convertFileBase64 that takes a file object as input and returns a Promise that resolves with the file contents encoded as a base64 data URL

--- client/src/components/DragDrop/CustomContainer.jsx
+++ client/src/components/DragDrop/CustomContainer.jsx
@@ -77,7 +77,21 @@ export function CustomDragDrop({
           });
         }
       }
- 
+
+      async function convertFileBase64(file) {
+        return new Promise((resolve, reject) => {
+          const reader = new FileReader();
+          reader.readAsDataURL(file);
+          reader.onload = () => {
+            resolve(reader.result);
+          };
+          reader.onerror = (error) => {
+            reject(error);
+          };
+        });
+      }
+      
+    
 
   return (
     <>
