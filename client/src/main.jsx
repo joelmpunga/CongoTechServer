46c43f3b3a4195ed7e0251dd722e3e468b133b83
paulinCiany <paulinlubamba24@gmail.com>
2024-04-05 00:44:08 +0200

    doc archiving and creating folder component finished

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import CreatFolder from './components/CreatFolder.jsx'
-import ArchDocs from './components/archDoc/ArchDocs.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CreatFolder />
+    <App />
   </React.StrictMode>,
 )
