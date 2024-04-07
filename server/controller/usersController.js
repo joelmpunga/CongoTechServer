5ce3c33737aac581416bb0deb3d14581060e3f06
paulinCiany <paulinlubamba24@gmail.com>
2024-04-07 13:32:47 +0200

    Create User Hashes the password using bcrypt_hash before saving it

--- server/controller/usersController.js
+++ server/controller/usersController.js
@@ -1,41 +1,28 @@
-import User from '../model/usersModel.js'
-export default class usersController {
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import User from '../model/usersModel.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
+const SECRET_KEY = process.env.SECRET_KEY;
+
+export default class usersController {
     static async createUser(req, res) {
         try {
             const { nom, postnom, email, password, role } = req.body;
-            const user = new User(nom, postnom, email, password, role);
+            const hashedPassword = await bcrypt.hash(password, 10); // Hash the password
+
+            const user = new User(nom, postnom, email, hashedPassword, role);
             const data = await user.signup();
-            const response = await user.getAll()
+            const response = await user.getAll();
             res.status(200).json(response);
-        }
-        catch (error) {
-            res.status(500).json(error);
-        }        
-    }
-
-    static async getAllUsers(req, res) {
-        // try{
-            const users = new User();
-            const data = await users.getAll()
-            res.status(200).json(data);
-            return data
-        // }
-        // catch (error) {
-        //     res.status(500).json(error);
-        // }
-    }
-
-    static async deleteUser(req,res){
-        const id = parseInt(req.params.id);
-        try{
-            const users = new User();
-            const data = await users.delete(id)
-            const response = await users.getAll()
-            res.status(200).json(response);
-        }
-        catch (error) {
+        } catch (error) {
             res.status(500).json(error);
         }
     }
+
+
+
+
+    
 }
