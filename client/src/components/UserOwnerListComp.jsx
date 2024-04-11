4007c10a63a7d6e75066e09beed68c1cf47ef9b1
paulinCiany <paulinlubamba24@gmail.com>
2024-04-11 12:42:22 +0200

    onwer list docs added

--- client/src/components/UserOwnerListComp.jsx
+++ client/src/components/UserOwnerListComp.jsx
@@ -1,5 +1,5 @@
 
-export default function UserOwnerListComp({ nom, typePostNom, descEmail, statusRole, thtypePostNom, thDecrEmail, thStatusRole, children }) {
+export default function UserOwnerListComp({thtypePostNom, thDecrEmail, thStatusRole, children }) {
 
 
     return (
@@ -15,35 +15,7 @@ export default function UserOwnerListComp({ nom, typePostNom, descEmail, statusR
                             <th scope="col" className="px-6 py-4 font-medium text-gray-900">Actions</th>
                         </tr>
                     </thead>
-                    <tbody className="divide-y divide-gray-100 border-t border-gray-100">
-                        <tr className="hover:bg-gray-50">
-                            <th className="flex gap-3 px-6 py-4 font-normal text-gray-900">
-                                <div className="text-sm">
-                                    <div className="font-medium text-gray-700">{nom}</div>
-                                </div>
-                            </th>
-                            <td className="px-6 py-4">
-                                <span className="inline-flex items-center gap-1 rounded-full  text-xs font-semibold">
-                                    {typePostNom}
-                                </span>
-                            </td>
-                            <td className="px-6 py-4">{descEmail}</td>
-                            <td className="px-6 py-4 ">
-                                <div className="">
-                                    <span className="inline-flex items-center gap-1 rounded-full bg-blue-50 px-2 py-1 text-xs font-semibold text-blue-600">
-                                        {statusRole}
-                                    </span>
-
-                                </div>
-                            </td>
-
-                            <td className='flex gap-3'>
-                                {children}
-                            </td>
-
-                        </tr>
-                        {/*  */}
-                    </tbody>
+                    {children}
                 </table>
             </div>
         </div>
