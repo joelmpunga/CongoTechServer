452e97b2f6893b20c1170f38a16f04e6ae30f6d8
Joel MPUNGA <joelmpunga79@gmail.com>
2024-04-16 15:07:28 +0200

    usecontext finished in login and folder components

--- client/src/components/SideBarSecretaire.jsx
+++ client/src/components/SideBarSecretaire.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Menu from './Menu'
 import ItemMenu from '../ui/ItemMenu'
 import LogoSideBar from '../ui/LogoSideBar'
@@ -10,23 +9,16 @@ export default function SideBarSecretaire() {
     <div className='bg-custom-dark-blue min-h-screen w-[430px] text-[20px] font-bold'>
       <LogoSideBar />
       <div className='overflow-x-hidden h-[70%]'>
-        
+
         <Menu title="Rapports & Statistiques" hasManyMenuItems={true} hasNumberCount={false} iconeRightOff="../src/assets/images/chevron-down.svg" iconeRightOn="../src/assets/images/chevron-up.svg" iconeLeft="../src/assets/images/Group.svg">
           <ItemMenu actived={true} title="Documents" />
           <ItemMenu actived={true} title="Emails" />
           <ItemMenu actived={true} title="Utilisteurs" />
           <ItemMenu actived={true} title="Clients" />
         </Menu>
-        <Menu title="Profil" hasManyMenuItems={false} hasNumberCount={false} iconeRightOff="../src/assets/images/chevron-down.svg" iconeRightOn="../src/assets/images/chevron-up.svg" iconeLeft="../src/assets/images/user-alt-4.svg" />
-        <Menu title="Dossiers" hasManyMenuItems={true} hasNumberCount={false} iconeRightOff="../src/assets/images/chevron-down.svg" iconeRightOn="../src/assets/images/chevron-up.svg" iconeLeft="../src/assets/images/list-alt.svg">
-          <Link to="/createfolder">
-            <ItemMenu actived={true} title="Création" />
-          </Link>
-          <Link to="/folder">
-            <ItemMenu actived={true} title="Dossiers" />
-          </Link>
-        </Menu>
-
+        <Link to="/folder">
+          <Menu title="Dossiers" hasManyMenuItems={false} hasNumberCount={false} iconeRightOff="../src/assets/images/chevron-down.svg" iconeRightOn="../src/assets/images/chevron-up.svg" iconeLeft="../src/assets/images/list-alt.svg"/>
+        </Link>
         <Menu title="Documents" hasManyMenuItems={true} hasNumberCount={false} iconeRightOff="../src/assets/images/chevron-down.svg" iconeRightOn="../src/assets/images/chevron-up.svg" iconeLeft="../src/assets/images/page.svg">
           <Link to="/archive">
             <ItemMenu actived={true} title="Archiver" />
