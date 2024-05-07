961ef9ec0e62d345577bf0b26a20bd9aa7fe2819
paulinCiany <paulinlubamba24@gmail.com>
2024-05-07 08:16:54 +0200

    style and notification added to folderclass component

--- client/src/components/FoldersClasser.jsx
+++ client/src/components/FoldersClasser.jsx
@@ -12,12 +12,24 @@ import { useNavigate } from 'react-router-dom';
 import { useMyContext } from '../contexts/MyContext';
 import ReactPaginate from 'react-paginate';
 import ActionBtns from './ActionBtns'
+import Swal from'sweetalert2'
 
 export default function FoldersClasser() {
-    
-    
+
+    const TopNotification = Swal.mixin({
+        toast: true,
+        position: "bottom-end",
+        showConfirmButton: false,
+        timer: 6000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener("mouseenter", Swal.stopTimer);
+            toast.addEventListener("mouseleave", Swal.resumeTimer);
+        }
+
+    });
     const [currentPage, setCurrentPage] = useState(0);
-    const [itemsPerPage] = useState(10); 
+    const [itemsPerPage] = useState(10);
     const handlePageClick = (data) => {
         const selectedPage = data.selected;
         setCurrentPage(selectedPage);
@@ -38,6 +50,19 @@ export default function FoldersClasser() {
     const [folders, setFolders] = useState([])
     const getFolders = async () => await axios.get("http://localhost:3000/folder").then(res => setFolders(res.data))
     useEffect(() => { getFolders() }, ['folders'])
+
+
+    if(folders.length > 0){
+        TopNotification.fire({
+            icon: "success",
+            title: "Archiver un ou plusieurs e-mails",
+            text:"Le(s) e-mails sont copiés avec succes dans le presse-papiers, veuillez selectionner le dossiers  et ensuite le sous dossier. S’ils n’existent, veuillez annuler et les créer d’abord",
+            width: 500,
+            height:300,
+            timer: 6000
+        });
+    }
+
     return (
         <>
             <HeaderWorkspace title="Classer Dossiers" >
@@ -45,14 +70,6 @@ export default function FoldersClasser() {
             </HeaderWorkspace>
             <WorkSpace message="Séléctionnez le dossier parent oû coller">
                 <div className='flex flex-wrap w-[100%] overflow-x-auto h-[70%]'>
-                    {/* {
-                        folders.map(folder => (
-                            <Link key={folder.id} to={{ pathname: `/subfolderclasser/${folder.id}/${idFile}`, state: { id: folder.id, idFile: idFile } }}>
-                                <Folder title={folder.titre} id={folder.id} />
-                            </Link>
-                        ))
-                    } */}
-
                     {
                         getCurrentPageData().map(folder => (
                             <Link key={folder.id} to={{ pathname: `/subfolderclasser/${folder.id}/${idFile}`, state: { id: folder.id, idFile: idFile } }}>
@@ -62,9 +79,7 @@ export default function FoldersClasser() {
                     }
                 </div>
 
-
-
-                <div className='flex justify-between w-full mx-5'>
+                <div className='flex justify-between items-center w-full mx-5'>
                     <ReactPaginate
                         previousLabel={"Précédent"}
                         nextLabel={"Suivant"}
@@ -76,23 +91,18 @@ export default function FoldersClasser() {
                         containerClassName={"flex justify-end gap-6 text-[20px]"}
                         activeClassName={"active"}
                     />
-
+                   
                     <div>
                         <Link to="/file/draft">
-
-                           
                             <ActionBtns
-                            className='flex flex-row justify-center items-center bg-red-600 rounded-2xl w-[150px] h-[50px] text-white'
-                            src="../src/assets/images/cancel-btn.svg"
-                            label="Annuler"
+                                className='flex flex-row justify-center items-center bg-red-600 rounded-2xl w-[150px] h-[50px] text-white'
+                                src="../src/assets/images/cancel-btn.svg"
+                                label="Annuler"
                             />
                         </Link>
                     </div>
                 </div>
             </WorkSpace>
-            {/* <div className='flex justify-end gap-2'>
-                <BouttonPagination />
-            </div> */}
             {/* <Popup /> */}
 
         </>
